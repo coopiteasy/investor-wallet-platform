e598734779521d6bff2d5f0050c0f997e764c6f6
Rémy Taymans <remytms@tsmail.eu>
2023-02-07 21:09:57 +0100

    linter

--- investor_wallet_platform_base/models/res_partner.py
+++ investor_wallet_platform_base/models/res_partner.py
@@ -298,21 +298,16 @@ class ResPartner(models.Model):
     @api.model
     def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
         """Filter the res.partner that are member of the logged in user structure."""
-        if (
-            self.env.context.get("restrict_to_user_structure")
-            and not (
-                self.env.user.has_group(
-                    "investor_wallet_platform_base.group_platform_wallet_user"
-                )
-                or self.env.user.has_group(
-                    "investor_wallet_platform_base.group_platform_wallet_manager"
-                )
+        if self.env.context.get("restrict_to_user_structure") and not (
+            self.env.user.has_group(
+                "investor_wallet_platform_base.group_platform_wallet_user"
+            )
+            or self.env.user.has_group(
+                "investor_wallet_platform_base.group_platform_wallet_manager"
             )
         ):
             domain = domain or []
-            domain += [
-                ("membership_structure_ids", "in", self.default_structure().ids)
-            ]
+            domain += [("membership_structure_ids", "in", self.default_structure().ids)]
         return super().search_read(domain, fields, offset, limit, order)
 
     @api.multi
